96df37007c93ddd70f672bcaf4490c716c76e425
Carlos <cep11p@gmail.com>
2025-07-18 23:25:03 -0300

    RH-73: actualizacion de seed, modelo y services para calculo de antiguedad

--- rrhh-backend/app/Services/LiquidacionService.php
+++ rrhh-backend/app/Services/LiquidacionService.php
@@ -130,8 +130,8 @@ class LiquidacionService
         $this->crearRemunerativo($liquidacion, $periodo, '002');
         //concepto adicional por titulo
         $this->crearRemunerativo($liquidacion, $periodo, '003');
-        //concepto adicional por antiguedad
-        $this->crearRemunerativo($liquidacion, $periodo, '004');
+
+        $liquidacion->calcularConceptoAntiguedad();
         //concepto adicional por zona
         $this->crearRemunerativo($liquidacion, $periodo, '005');
     }
@@ -186,13 +186,6 @@ class LiquidacionService
 
         $concepto = Concepto::where('codigo', $codigo)->first();
         $valor_concepto = $concepto->valorConcepto($periodo);
-
-        //si es el concepto de antiguedad, se tiene que calcular el importe de la antiguedad
-        if($concepto->codigo == '004'){
-            $pre_antiguedad = $liquidacion->empleado->pre_antiguedad;
-            $antiguedad = $liquidacion->empleado->antiguedad + $pre_antiguedad;
-            $valor_concepto = $valor_concepto->valor * $antiguedad;
-        }
 
         if (!$valor_concepto) {
             throw new \Exception("No existe un valor para el concepto {$concepto->codigo} en el período {$periodo}");
